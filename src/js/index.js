bba9b6b779ab5074440d1e2c49a670821ee0888f
Michał Szeniak <michal.szeniak@gmail.com>
2020-06-10 18:27:39 +0200

    feat: about

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,16 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+const burger = document.querySelector(".burger-js");
+const list = document.querySelector(".navigation__list");
+const circle = document.querySelector(".circle");
+const close = document.querySelector(".button__close");
+
+
+burger.addEventListener('click', ()=> {
+    list.classList.toggle("navigation__open");
+    circle.classList.toggle("circle__open");
+    close.classList.toggle("button__close--open");
+});
